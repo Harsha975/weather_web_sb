e22973141290de5480150d46dd7e8ffc01e1cdc7
Harsha975 <96220912+Harsha975@users.noreply.github.com>
2025-12-07 12:51:43 +0530

    first-commit

---
+++ src/main/java/com/weather/weatheApi/WeatheApiApplication.java
@@ -0,0 +1,12 @@
+package com.weather.weatheApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatheApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatheApiApplication.class, args);
+	}
+}
